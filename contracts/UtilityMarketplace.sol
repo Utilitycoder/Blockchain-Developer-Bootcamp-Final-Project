824ba86ef5b44813899139a597b6abb5f7e48342
Utilitycoder <66361605+Utilitycoder@users.noreply.github.com>
2022-01-06 00:38:36 +0100

    Update

---
+++ contracts/UtilityMarketplace.sol
@@ -0,0 +1,183 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.11;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+
+/// @title UtilityMarketplace
+/// @author Lawal Abubakar Babatunde
+/// @dev This contract is used to create a marketplace for the Eternal NFTs.
+/// @dev Inherits ReentrancyGuard which is deployed on createEternalMarketItem() and createEternalItemSale() functions
+/// @dev Uses Counters library for tracking itemID for the marketplace items created.
+/// @dev Uses Counters library for tracking number of items Sold.
+contract UtilityMarketplace is ReentrancyGuard {
+    using Counters for Counters.Counter;
+    Counters.Counter private _itemNo;
+    Counters.Counter private _itemSold;
+
+    address payable owner;
+    uint256 initialPrice = 0.02 ether;
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    struct EternalItem {
+        uint256 itemNo;
+        address nftContract;
+        uint256 tokenId;
+        address payable seller;
+        address payable owner;
+        uint256 price;
+        bool sold;
+    }
+
+    mapping(uint256 => EternalItem) private idToEternalItem;
+
+    event EternalItemCreated (
+        uint256 indexed itemNo,
+        address indexed nftContract,
+        uint256 indexed tokenId,
+        address seller,
+        address owner,
+        uint256 price,
+        bool sold
+    );
+
+    /// @notice Returns the initial price for listing an item on the marketplace
+    /// @dev 0.02 ether is what I chose, you can use whatever you want.
+    /// @return uint256 The listing price
+    function getInitialPrice() public view returns(uint256) {
+        return initialPrice;
+    }
+
+    /// @notice Creates an item on the marketplace
+    /// @dev transfers the token from owner to the marketplace contract
+    /// @param nftContract Address of the NFT contract
+    /// @param tokenId Token ID of the NFT
+    /// @param price The amount the seller wants to sell the NFT
+    function createUtilityMarketItem(address nftContract , uint256 tokenId, 
+        uint256 price) public payable nonReentrant {
+            
+        require(price > 0, "Price must be greater than 0");
+        require(msg.value == initialPrice, "Price must be equal to listing price");
+
+        uint256 itemNo = _itemNo.current();
+
+        idToEternalItem[itemNo] = EternalItem(
+            itemNo,
+            nftContract,
+            tokenId,
+            payable(msg.sender),
+            payable(address(0)),
+            price,
+            false
+        );
+
+        IERC721(nftContract).transferFrom(msg.sender, address(this), tokenId);
+
+        _itemNo.increment();
+
+        emit EternalItemCreated(
+            itemNo,
+            nftContract,
+            tokenId,
+            msg.sender,
+            address(0),
+            price,
+            false
+        );
+    }
+
+    /// @notice Creates an item sale on the marketplace
+    /// @dev Transfers the token from the marketplace contract to the buyer
+    /// @dev Transfer eth from the buyer to the seller of the token through the marketplace contract  
+    /// @param nftContract Address of the NFT contract
+    /// @param itemNo ID of the item to be sold
+    function createItemSale(
+        address nftContract, 
+        uint256 itemNo
+        ) public payable nonReentrant {
+
+        uint256 tokenId = idToEternalItem[itemNo].tokenId;
+        uint256 price = idToEternalItem[itemNo].price;
+
+        require(msg.value == price, "Please make sure the amount to be paid is equal to the listed price");
+
+        (bool success, ) = idToEternalItem[itemNo].seller.call{value: msg.value}("");
+        require(success, "Transfer failed");
+
+        IERC721(nftContract).transferFrom(address(this), msg.sender, tokenId);
+
+        idToEternalItem[itemNo].owner = payable(msg.sender);
+        idToEternalItem[itemNo].sold = true;
+        _itemSold.increment();
+
+        payable(owner).transfer(initialPrice);
+    }
+
+    /// @notice Returns the details of all the unsold marketplace items
+    /// @return EternalItem[] All the unsold marketplace items
+
+    function fetchItems() public view returns(EternalItem[] memory) {
+        uint256 itemCount = _itemNo.current();
+        uint256 unsoldItemCount = _itemNo.current() - _itemSold.current();
+        uint256 currentIndex = 0;
+
+        EternalItem[] memory items = new EternalItem[](unsoldItemCount);
+        
+        for(uint256 i = 0; i < itemCount; i++) {
+            if (idToEternalItem[i].owner == address(0)) {
+                uint256 currentId = i;
+                EternalItem storage currentItem = idToEternalItem[currentId];
+                items[currentIndex] = currentItem;
+                currentIndex += 1;
+            }
+        }
+
+        return items;
+    }
+
+    /// @notice Returns the details of the unSold marketplace items by ItemId
+    /// @param itemId ID of the item to be fetched
+    /// @return EternalItem The unsold marketplace item
+    function fetchEternalItemById(uint256 itemId) public view returns(EternalItem memory) {
+        return idToEternalItem[itemId];
+    }
+
+    /// @notice Returns the details of the marketplace items owned by the owner
+    /// @return EternalItem[] All the marketplace items owned by the owner
+    function fetchMyEternalItems() public view returns(EternalItem[] memory) {
+        uint totalItemCount = _itemNo.current();
+        uint itemCount = 0;
+        uint currentIndex = 0;
+
+        for (uint i = 0; i < totalItemCount; i++) {
+            if (idToEternalItem[i].owner == msg.sender) {
+                itemCount += 1;
+            }
+        }
+
+        EternalItem[] memory items = new EternalItem[](itemCount);
+
+        for (uint i = 0; i < totalItemCount; i++) {
+            if (idToEternalItem[i].owner == msg.sender) {
+                uint currentId = i;
+                EternalItem storage currentItem = idToEternalItem[currentId];
+                items[currentIndex] = currentItem;
+                currentIndex += 1;
+            }
+        }
+
+        return items;
+    }
+
+    ///@dev receive() and fallback() functions to allow the contract to receive ETH and data  
+    receive() external payable {
+    }
+
+    fallback() external payable {
+    }
+}
---
+++ test/UtilityNFTtest.js
@@ -0,0 +1,201 @@
+const { assert } = require('chai')
+
+describe('UtilityNFT Contract', async () => {
+	let nft
+	let market
+	let marketContractAddress
+	let nftContractAddress
+	let tokenId
+
+	// Deploys the UtilityNFT contract and the UtilityMarket contract before each test
+	beforeEach('Setup Contract', async () => {
+		const Market = await ethers.getContractFactory('UtilityMarketplace')
+		market = await Market.deploy()
+		await market.deployed()
+		marketContractAddress = await market.address
+
+		const UtilityNFT = await ethers.getContractFactory('UtilityNFT')
+		nft = await UtilityNFT.deploy(marketContractAddress)
+		await nft.deployed()
+		nftContractAddress = await nft.address
+	})
+
+	// Tests address for the UtilityNFT contract
+	it('Should have an address', async () => {
+		assert.notEqual(nftContractAddress, 0x0)
+		assert.notEqual(nftContractAddress, '')
+		assert.notEqual(nftContractAddress, null)
+		assert.notEqual(nftContractAddress, undefined)
+	})
+
+	// Tests name for the token of UtilityNFT contract
+	it('Should have a name', async () => {
+		// Returns the name of the token
+		const name = await nft.collectionName()
+
+		assert.equal(name, 'UtilityNFT')
+	})
+
+	// Tests symbol for the token of UtilityNFT contract
+	it('Should have a symbol', async () => {
+		// Returns the symbol of the token
+		const symbol = await nft.collectionSymbol()
+
+		assert.equal(symbol, 'UNFT')
+	})
+
+	// Tests for NFT minting function of UtilityNFT contract using tokenID of the minted NFT
+	it('Should be able to mint NFT', async () => {
+		// Mints a NFT
+		let txn = await nft.createUtilityNFT()
+		let tx = await txn.wait()
+
+		// tokenID of the minted NFT
+		let event = tx.events[0]
+		let value = event.args[2]
+		tokenId = value.toNumber()
+
+		assert.equal(tokenId, 0)
+
+		// Mints another NFT
+		txn = await nft.createUtilityNFT()
+		tx = await txn.wait()
+
+		// tokenID of the minted NFT
+		event = tx.events[0]
+		value = event.args[2]
+		tokenId = value.toNumber()
+
+		assert.equal(tokenId, 1)
+	})
+
+	// Test for number of NFTs owned by an address
+	it('Should be able to return number of NFTs owned by and address', async () => {
+		// Mints a NFT
+		let txn = await nft.createUtilityNFT()
+		await txn.wait()
+
+		// Returns the array of NFTs owned by the address
+		let tokensOwned = await nft.getMyUtilityNFT()
+
+		assert.equal(tokensOwned.length, 1)
+	})
+})
+
+describe('EternalMarketpalce Contract', function () {
+	let nft
+	let market
+	let marketContractAddress
+	let nftContractAddress
+	let listingPrice
+	let auctionPrice
+
+	// Deploys the UtilityNFT contract and the UtilityMarket contract before each test
+	beforeEach('Eternal Marketplace', async () => {
+		const Market = await ethers.getContractFactory('UtilityMarketplace')
+		market = await Market.deploy()
+		await market.deployed()
+		marketContractAddress = await market.address
+
+		const UtilityNFT = await ethers.getContractFactory('UtilityNFT')
+		nft = await UtilityNFT.deploy(marketContractAddress)
+		await nft.deployed()
+		nftContractAddress = await nft.address
+
+		listingPrice = await market.getInitialPrice()
+		listingPrice = listingPrice.toString()
+
+		auctionPrice = ethers.utils.parseUnits('1', 'ether')
+	})
+
+	// Test for creation of an Utility Marketplace item
+	it('Should be able to create an NFT Item', async () => {
+		// Mints a NFT
+		await nft.createUtilityNFT()
+
+		// Puts the NFT up for sale in the eternal marketplace
+		await market.createUtilityMarketItem(nftContractAddress, 0, auctionPrice, {
+			value: listingPrice,
+		})
+
+		// Fetches the remaining unsold marketplace items
+		let items = await market.fetchItems()
+
+		assert.equal(items.length, 1)
+	})
+
+// 	// Test for creation and sale of an Eternal Marketplace item
+// 	it('Should be able to execute Eternal Item Sale', async () => {
+// 		// Mints 2 NFTs
+// 		await nft.createUtilityNFT()
+// 		await nft.createUtilityNFT()
+
+// 		// Puts the first NFT up for sale in the eternal marketplace
+// 		await market.createEternalMarketItem(nftContractAddress, 0, auctionPrice, {
+// 			value: listingPrice,
+// 		})
+
+// 		// Puts the second NFT up for sale in the eternal marketplace
+// 		await market.createEternalMarketItem(nftContractAddress, 1, auctionPrice, {
+// 			value: listingPrice,
+// 		})
+
+// 		const [_, buyerAddress] = await ethers.getSigners()
+
+// 		// Creates a sale for the first NFT and transfers it from the owner to the buyer through the marketplace contract
+// 		await market
+// 			.connect(buyerAddress)
+// 			.createEternalItemSale(nftContractAddress, 1, { value: auctionPrice })
+
+// 		// Fetches the remaining unsold marketplace items
+// 		// Returns one as one of the two NFT minted is sold
+// 		let items = await market.fetchEternalItems()
+
+// 		assert.equal(items.length, 1)
+// 	})
+
+// 	// Test for fetchng details of an Eternal Marketplace item using its itemId
+// 	it('Should be able to get an Eternal item by its tokenId', async () => {
+// 		// Mints 2 NFTs
+// 		await nft.createUtilityNFT()
+// 		await nft.createUtilityNFT()
+
+// 		// Puts the first NFT up for sale in the eternal marketplace
+// 		await market.createEternalMarketItem(nftContractAddress, 0, auctionPrice, {
+// 			value: listingPrice,
+// 		})
+
+// 		// Puts the second NFT up for sale in the eternal marketplace
+// 		await market.createEternalMarketItem(nftContractAddress, 1, auctionPrice, {
+// 			value: listingPrice,
+// 		})
+
+// 		// Fetches the details of first marketplace item by its itemId
+// 		let item = await market.fetchEternalItemById(1)
+
+// 		assert.equal(item.itemId, 1)
+// 	})
+
+// 	// Test for fetchng details of all created Eternal Marketplace items
+// 	it('Should be able to get an Eternal item by its tokenId', async () => {
+// 		// Mints 2 NFTs
+// 		await nft.createUtilityNFT()
+// 		await nft.createUtilityNFT()
+
+// 		// Puts the first NFT up for sale in the eternal marketplace
+// 		await market.createEternalMarketItem(nftContractAddress, 0, auctionPrice, {
+// 			value: listingPrice,
+// 		})
+
+// 		// Puts the second NFT up for sale in the eternal marketplace
+// 		await market.createEternalMarketItem(nftContractAddress, 1, auctionPrice, {
+// 			value: listingPrice,
+// 		})
+
+// 		// Fetches the details of all unsold marketplace items
+// 		// Returs 2 as two eternal items are created and none is sold
+// 		let item = await market.fetchEternalItems()
+
+// 		assert.equal(item.length, 2)
+// 	})
+})
