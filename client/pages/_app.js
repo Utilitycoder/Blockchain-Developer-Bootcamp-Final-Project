be11b529337cebaea46682b859c06b2ef7e9d56a
Utilitycoder <66361605+Utilitycoder@users.noreply.github.com>
2022-01-11 04:06:14 +0100

    Update

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -114,7 +114,7 @@ function MyApp({ Component, pageProps }) {
 						</svg>
 					</div>
 					<div className='text-3xl font-extrabold'>
-						Welcome to Eternal Domain
+						Welcome to Utility NFT Marketplace
 					</div>
 					<button
 						className='text-2xl font-bold py-3 px-12 bg-green-800 text-gray-100 shadow-xl rounded-lg mb-10 hover:scale-105 transition duration-500 ease-in-out'
